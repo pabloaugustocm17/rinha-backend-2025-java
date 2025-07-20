8db347d113ebaff48ae9cd08af650741acb7c72a
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2025-07-20 07:28:33 -0300

    feat: initial commit

---
+++ src/test/java/com/rinha/coffe/CoffeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinha.coffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
